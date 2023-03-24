1e37aeca7d31aed139165acc89f256ee8a436ba3
Reed W Priest <pries153@umn.edu>
2023-03-23 22:36:59 -0500

    Added comments describing each line of code.

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -6,10 +6,12 @@ library(jsonlite)
 
 ## Data Import and Cleaning
 
-# JSON last downloaded on 03/23/2023 at 8:38pm
+# JSON last downloaded on 03/23/2023 at 8:38pm. This downloads the JSON file so that we can later extract information from it.
 rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE)
+# This line turns the extracted JSON into a legible tibble for easy analyses. I specified the path data to children because, using the JSON viewer Chrome extension, I could see that the information we want were nested under this path sequence.
 rstats_original_tbl <- as_tibble(rstats_list$data$children)
 
+# This series of pipes creates the three columns relevant for our analyses. I ended the pipe by selecting only the relevant columns. The previous tibble had over 100 columns, which is visually unweildy, so this simple tibble works well.
 rstats_tbl <- rstats_original_tbl %>%
   mutate(post = data.title,
          upvotes = data.ups,
@@ -17,24 +19,30 @@ rstats_tbl <- rstats_original_tbl %>%
   select(post:comments)
 
 ## Visualization
+# This series of pipes creates our ggplot visualizing the scatterplot of the number of upvotes to the number of comments. I first thought I would jitter, bu no points are really overlapping, so jittering would only add confusion. I saved the figure to the figs folder so that it could be easily viewed on Github.
 (ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
   geom_point() +
   labs(x = "Upvotes", y = "Comments"))  %>%
   ggsave("../figs/fig1.png", ., width = 1920, height = 1080, units = "px")
 
 ## Analysis
+# This line conducts a correlation test between the number of upvotes and the number of comments to see if they are significantly related.
 cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
-df_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[2]] 
+# I chose to define the degrees of freedom, r-value, and p-value for the above statistical test to enable cleaner code in the Publications section. This line defines our degrees of freedom.
+df_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[2]]
+# This series of pipes defines our r-value produced by the above test. I had to round values and remove the leading zero to fit APA style.
 r_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[4]] %>%
   round(2) %>%
   str_remove(pattern = "^(?-)0")
+# This series of pipes defines our p-value produced by the above test. I had to round values and remove the leading zero to fit APA style.
 p_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[3]] %>%
   round(2) %>%
   format(nsmall = 2) %>%
   str_remove(pattern = "^(?-)0")
 
 ## Publication
-paste0("The correlation between upvotes and comments was r(", df_value, ") = ", r_value, ", p = ", p_value, ". This value ", if (p_value < .05) {"was"} else ("was not"), " statistically significant.")
+# This line takes the previously defined degrees of freedom, r-value, and p-value of our correlation test and combines them to form a coherent sentence interpreting the statistical test. The line is dynamic such that values and the "was/was not significant" text change to fit the statistics that were calculated in line 30.
+paste0("The correlation between upvotes and comments was r(", df_value, ") = ", r_value, ", p = ", p_value, ". This test ", if (p_value < .05) {"was"} else ("was not"), " statistically significant.")
 
 
 # What I generally need to do:
