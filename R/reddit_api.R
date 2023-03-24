c4366b0c0888eb223cee2a049cdf60a54dc34dc6
Reed W Priest <pries153@umn.edu>
2023-03-24 14:31:40 -0500

    Added greater clarity to comments.

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -6,8 +6,9 @@ library(jsonlite)
 
 ## Data Import and Cleaning
 
-# JSON last downloaded on 03/23/2023 at 8:38pm. This downloads the JSON file so that we can later extract information from it.
+# JSON last downloaded on 03/24/2023 at 2:07pm. This downloads the JSON file so that we can later extract information from it.
 rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE)
+
 # This line turns the extracted JSON into a legible tibble for easy analyses. I specified the path data to children because, using the JSON viewer Chrome extension, I could see that the information we want were nested under this path sequence.
 rstats_original_tbl <- as_tibble(rstats_list$data$children)
 
@@ -28,12 +29,15 @@ rstats_tbl <- rstats_original_tbl %>%
 ## Analysis
 # This line conducts a correlation test between the number of upvotes and the number of comments to see if they are significantly related.
 cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
+
 # I chose to define the degrees of freedom, r-value, and p-value for the above statistical test to enable cleaner code in the Publications section. This line defines our degrees of freedom.
 df_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[2]]
+
 # This series of pipes defines our r-value produced by the above test. I had to round values and remove the leading zero to fit APA style.
 r_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[4]] %>%
   round(2) %>%
   str_remove(pattern = "^(?-)0")
+
 # This series of pipes defines our p-value produced by the above test. I had to round values and remove the leading zero to fit APA style.
 p_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[3]] %>%
   round(2) %>%
@@ -41,13 +45,5 @@ p_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[3]] %>%
   str_remove(pattern = "^(?-)0")
 
 ## Publication
-# This line takes the previously defined degrees of freedom, r-value, and p-value of our correlation test and combines them to form a coherent sentence interpreting the statistical test. The line is dynamic such that values and the "was/was not significant" text change to fit the statistics that were calculated in line 30.
+# This line takes the previously defined degrees of freedom, r-value, and p-value of our correlation test and combines them to form a coherent sentence interpreting the statistical test. The line is dynamic such that values and the "was/was not significant" text change to fit the statistics that were calculated in line 31.
 paste0("The correlation between upvotes and comments was r(", df_value, ") = ", r_value, ", p = ", p_value, ". This test ", if (p_value < .05) {"was"} else ("was not"), " statistically significant.")
-
-
-# What I generally need to do:
-# 5) Identify what parts of reddit are important
-# 7) Read those parts of reddit into a tibble
-# 8) Make a ggplot based on that tibble and make a correlation
-# 9) Add descriptions explaining each of my steps in each document
-# 10) Check work in each document
--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -5,17 +5,21 @@ library(tidyverse)
 library(rvest)
 
 ## Data Import and Cleaning
-# Imported on 03/24/2023 at 1:43pm
+# Imported on 03/24/2023 at 1:43pm. This line imports the data in an html format to later be scraped.
 rstats_html <- read_html("https://old.reddit.com/r/rstats/")
 
+#This series of pipes creates our post variable by defining an xpath. I found this xpath and the xpaths for the next two variables by right-clicking the desired element and then clicking "inspect" to see its xpath information.
 post <- rstats_html %>%
   html_elements(xpath = "//a[@data-event-action = 'title']") %>%
   html_text()
 
+#This series of pipes creates our upvotes variable by defining an xpath. I had to convert it into numeric so that it could be used in later analyses.
 upvotes <- rstats_html %>%
   html_elements(xpath = "//div[@class = 'score unvoted']") %>%
-  html_text()
+  html_text() %>%
+  as.numeric()
 
+# This series of pipes creates our comments variable by defining an xpath. I had to remove the end of each element so that "9 comments" became "9" for later analyses. I added zeros for posts which had no comments by replacing NAs.
 comments <- rstats_html %>%
   html_elements(xpath = "//a[@data-event-action = 'comments']") %>%
   html_text() %>%
@@ -23,5 +27,34 @@ comments <- rstats_html %>%
   as.numeric() %>%
   replace_na(0)
 
+# This line combines our three previously created variables to create our tibble we will use in later analyses. Importantly, both upvotes and comments were stored as dbls to enable analysis.
 rstats_tbl <- tibble(post, upvotes, comments)
-# Error here because there are only 22 comments, but 25 of each other variable.  
+
+## Visualization
+# This series of pipes creates our ggplot visualizing the relationship between the number of upvotes with the number of comments. I saved the output to the figs folder to easily refer back to it later if needed.
+(ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
+    geom_point() +
+    labs(x = "Upvotes", y = "Comments"))  %>%
+  ggsave("../figs/fig2.png", ., width = 1920, height = 1080, units = "px")
+
+## Analysis
+# This line conducts a correlation test between the number of upvotes and the number of comments to see if they are significantly related.
+cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
+
+# I chose to define the degrees of freedom, r-value, and p-value for the above statistical test to enable cleaner code in the Publications section. This line defines our degrees of freedom.
+df_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[2]]
+
+# This series of pipes defines our r-value produced by the above test. I had to round values and remove the leading zero to fit APA style.
+r_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[4]] %>%
+  round(2) %>%
+  str_remove(pattern = "^(?-)0")
+
+# This series of pipes defines our p-value produced by the above test. I had to round values and remove the leading zero to fit APA style.
+p_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[3]] %>%
+  round(2) %>%
+  format(nsmall = 2) %>%
+  str_remove(pattern = "^(?-)0")
+
+## Publication
+# This line of text brings in our previously defined values to construct a dynamic sentence that interprets the correlation test. 
+paste0("The correlation between upvotes and comments was r(", df_value, ") = ", r_value, ", p = ", p_value, ". This test ", if (p_value < .05) {"was"} else ("was not"), " statistically significant.")
