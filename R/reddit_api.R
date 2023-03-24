425f7dc5ac3680e0d0a24c6fb765cd9e49320083
Reed W Priest <pries153@umn.edu>
2023-03-23 20:33:41 -0500

    JSON first run done. Check later.

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,10 +1,10 @@
-# Script Settings and Resources
+## Script Settings and Resources
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(jsonlite)
 
-# Data Import and Cleaning
+## Data Import and Cleaning
 
 # JSON downloaded on 03/23/2023 at 7pm.
 rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE)
@@ -16,22 +16,27 @@ rstats_tbl <- rstats_original_tbl %>%
          comments = data.num_comments) %>%
   select(post:comments)
 
-# Visualization
-# ggplot here
+## Visualization
+(ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
+  geom_point() +
+  labs(x = "Upvotes", y = "Comments"))  %>%
+  ggsave("../figs/fig1.png", ., width = 1920, height = 1080, units = "px")
 
+## Analysis
+cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
+df_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[2]] 
+r_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[4]] %>%
+  round(2) %>%
+  str_remove(pattern = "^(?-)0")
+p_value <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)[[3]] %>%
+  round(2) %>%
+  format(nsmall = 2) %>%
+  str_remove(pattern = "^(?-)0")
 
-# Analysis
-# r-value calculation here
-
-
-# Publication
-# r-value interpretation here 
-
+## Publication
+paste0("The correlation between upvotes and comments was r(", df_value, ") = ", r_value, ", p = ", p_value, ". This value was not statistically significant.")
 
 # What I generally need to do:
-# 1) Import the JSON
-# 2) Identify what pieces of the JSON to read
-# 3) Read those parts of the JSON to assign them to a tibble
 # 4) Make a ggplot based on that tibble and make a correlation
 # 5) Identify what parts of reddit are important
 # 6) Scrape reddit
